a948bf8bfdd3d66088d67e82e26ce64d4626b6d6
gabriel1325t <gabriel.carrasco1325@gmail.com>
2025-04-16 16:20:02 -0300

    16-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import ListEvento from "./pages/listEvento";
 import ListUsers from "./pages/listUsers";
 import Login from "./pages/Login";
 import Cadastro from "./pages/Cadastro";
@@ -11,14 +11,34 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/cadastro" element={<ProtectedRoute><Cadastro /></ProtectedRoute>} />
-          <Route path="/users" element={<ProtectedRoute>
-            <ListUsers/>
-            </ProtectedRoute>
-          } />
+          <Route
+            path="/cadastro"
+            element={
+              <ProtectedRoute>
+                <Cadastro />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/users"
+            element={
+              <ProtectedRoute>
+                <ListUsers />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/eventos"
+            element={
+              <ProtectedRoute>
+                <ListEvento />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
 export default App;
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -5,6 +5,8 @@ function Cadastro() {
       <h1>CADASTRO</h1>
       <Link to="/users"> Ir para lista de usuários </Link>
     </div>
+    
   );
+  
 }
 export default Cadastro;
